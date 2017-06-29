185432b206f2cb41ffb7aa10b2d67fd2865dfaf7
kingjohnny <johnny@thinktomorrow.be>
2017-06-29 08:52:27 +0200

    Midsummer init

---
+++ README.md
@@ -0,0 +1,14 @@
+<img src="assets/img/logo.png" alt="Teepee"/>
+
+## Getting started:
+1. clone the teepee repository from `github.com:kingjohnny/teepee.git`.
+2. Run `yarn install` to install all dependencies. This will install laravel mix which uses webpack behind the scenes.
+3. To build your sass and js assets, run `npm run dev`.
+4. To watch for changes, run `npm run watch`.
+5. Have a pow wow.
+
+## Dependencies:
+- Warpaint
+- Animate.css
+- Jquery
+- Font-awesome
---
+++ public/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+  <!-- search meta -->
+  <title>Midsummer Madness - Feestvarken VZW</title>
+  <link rel="stylesheet" href="assets/css/app.css" />
+  <script src="assets/js/head.js"></script>
+
+  <script src="https://use.fontawesome.com/0a6480ae96.js"></script>
+
+  <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
+
+
+</head>
+<body>
+  <img src="assets/img/flag-left.png" class="flag-left">
+  <img src="assets/img/flag-right.png" class="flag-right">
+  <main class="container">
+    <div class="row center-center">
+      <div class="column-6 stretched-xl">
+        <img src="assets/img/logo.png" alt="kids">
+      </div>
+    </div>
+    <div class="buttons row stack-l">
+      <a data-fancybox target="_blank" href="assets/pdf/feestvarken-instagramguide.pdf" class="btn column-2">
+        <i class="fa fa-file-pdf-o"></i>
+        Instaguide
+      </a>
+      <a data-fancybox target="_blank" href="https://vimeo.com/222536333?color=c61870" class="btn column-2">
+        <i class="fa fa-film"></i>
+        Facebook cover
+      </a>
+      <a data-fancybox data-src="https://feestvarken.thinktomorrow.be" class="btn column-2" href="javascript:;">
+        <i class="fa fa-television"></i>
+        Website
+      </a>
+      <a data-fancybox="iframe" data-src="https://feestvarken.thinktomorrow.be/particulier/" data-type="iframe" class="btn column-2" href="javascript:;">
+        <i class="fa fa-credit-card"></i>
+        Donaties
+      </a>
+      <a data-fancybox="iframe" data-src="http://us2.campaign-archive1.com/?e=&u=7e7cd7eb953d2f6917cc428de&id=eff88218a0" data-type="iframe" class="btn column-2" href="javascript:;">
+        <i class="fa fa-envelope-o"></i>
+        E-mail
+      </a>
+
+    </div>
+  </main>
+
+
+  <footer class="text-center">
+    <img src="assets/img/kids.png" alt="kids">
+  </footer>
+  <!-- foot scripts -->
+  <script src="assets/js/app.js"></script>
+  <script>
+  ;(function(){
+    $("[data-fancybox]").fancybox({
+      iframe : {
+        attr : {
+          scrolling : 'yes'
+        }
+      }
+    });
+  });
+  </script>
+
+</body>
+</html>
